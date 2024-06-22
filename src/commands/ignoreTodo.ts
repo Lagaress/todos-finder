148ada71973a082c1ad259d51808a180825410c9
jesuslagares <jesus.lagares@novatalent.com>
2024-06-22 18:12:16 +0200

    MOD: ignoreTodoCommand refactored and cleaned

--- src/commands/ignoreTodo.ts
+++ src/commands/ignoreTodo.ts
@@ -1,18 +1,33 @@
-import * as vscode from 'vscode';
-import todoService from '../services/todoService';
+import * as vscode from "vscode";
+import todoService from "../services/todoService";
+import config from "../config/config";
 
-function ignoreTodoCommand() {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-        const position = editor.selection.active;
-        const line = editor.document.lineAt(position.line);
+async function ignoreTodoCommand() {
+  console.log("ignoreTodoCommand: Command execution started.");
+  const activeTextEditor = vscode.window.activeTextEditor;
+  if (!activeTextEditor) {
+    console.error("ignoreTodoCommand: Active editor not found.");
+    return;
+  }
 
-        editor.edit(editBuilder => {
-            editBuilder.insert(new vscode.Position(line.lineNumber, 0), '//ignore-todo\n');
-        }).then(() => {
-            todoService.updateTodoCount();
-        });
-    }
-};
+  const currentPosition = activeTextEditor.selection.active;
+  const currentLine = activeTextEditor.document.lineAt(currentPosition.line);
+  const ignoreTodoComment = config.IGNORE_TODO_COMMENT;
+
+  console.log(`ignoreTodoCommand: Inserting ${ignoreTodoComment} at line ${currentLine.lineNumber}.`);
+  try {
+    await activeTextEditor.edit((editBuilder) => {
+      const insertPosition = new vscode.Position(currentLine.lineNumber, 0);
+      editBuilder.insert(insertPosition, `${ignoreTodoComment}\n`);
+    });
+
+    console.log(`ignoreTodoCommand: ${ignoreTodoComment} inserted successfully, updating TODO count.`);
+    await todoService.updateTodoCount();
+
+    console.log("ignoreTodoCommand: Command execution finished.");
+  } catch (error) {
+    console.error(`ignoreTodoCommand: Error occurred while inserting ${ignoreTodoComment}:`, error);
+  }
+}
 
 export default ignoreTodoCommand;
---
+++ src/config/config.ts
@@ -0,0 +1,9 @@
+type TodosFinderConfig = {
+  IGNORE_TODO_COMMENT: string;
+}
+
+const config: TodosFinderConfig = {
+  IGNORE_TODO_COMMENT: "//ignore-todo",
+}
+
+export default config;
