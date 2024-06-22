a0d589357ac21a2e2e00536c443c0d7ea3569e50
jesuslagares <jesus.lagares@novatalent.com>
2024-06-22 18:14:20 +0200

    MOD: refreshTodosCommand improved

--- src/commands/refreshTodos.ts
+++ src/commands/refreshTodos.ts
@@ -1,7 +1,9 @@
 import todoService from '../services/todoService';
 
-function refreshTodosCommand() {
-  todoService.updateTodoCount();
+async function refreshTodosCommand(): Promise<void> {
+  console.log("refreshTodosCommand: Command execution started.");
+  await todoService.updateTodoCount();
+  console.log("refreshTodosCommand: Command execution finished.");
 };
 
 export default refreshTodosCommand;
